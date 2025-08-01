c4cef5a0a0072ef0f45f804fa1351bf15569a19c
Caleb <164523858+CalebTahane@users.noreply.github.com>
2025-08-01 03:06:26 +0100

    commit

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,8 @@
 // function handleClick () {
 //     alert("I got clicked!")
 // }
+
+//This part is responsible for the button press effects
 let buttons = document.querySelectorAll(".drum")
 
 for (let i = 0; i < buttons.length; i++) {
@@ -11,47 +13,56 @@ for (let i = 0; i < buttons.length; i++) {
         // const audio = new Audio('./sounds/tom-1.mp3')
         // audio.play()
         let buttonInnerHTML = this.innerHTML
-
-        switch (buttonInnerHTML) {
-            case "w":
-                const tom1 = new Audio('./sounds/tom-1.mp3')
-                tom1.play()   
-            break
-
-            case "a":
-                const tom2 = new Audio('./sounds/tom-2.mp3')
-                tom2.play()  
-            break
-
-            case "s":
-                const tom3 = new Audio('./sounds/tom-3.mp3')
-                tom3.play()  
-            break
-
-            case "d":
-                const tom4 = new Audio('./sounds/tom-4.mp3')
-                tom4.play()  
-            break
-
-            case "j":
-                const snare = new Audio('./sounds/snare.mp3')
-                snare.play()
-            break
-
-            case "k":
-                const crash = new Audio('./sounds/crash.mp3')
-                crash.play()
-            break
-
-            case "l":
-                const kick = new Audio('./sounds/kick-bass.mp3')
-                kick.play()
-            break
-
-            default: console.log(buttonInnerHTML)
-        }
+        makeSound(buttonInnerHTML)       
     })
 }
 // document.querySelector(".drum").addEventListener("click", function () {
 //     alert("I got clicked !")
 // });
+
+//This part is responsible for the key press
+document.addEventListener("keydown", function(e) {
+    makeSound(e.key)
+})
+
+//function that makes sound to be played
+function makeSound (key) {
+    switch (key) {
+        case "w":
+            const tom1 = new Audio('./sounds/tom-1.mp3')
+            tom1.play()   
+        break
+
+        case "a":
+            const tom2 = new Audio('./sounds/tom-2.mp3')
+            tom2.play()  
+        break
+
+        case "s":
+            const tom3 = new Audio('./sounds/tom-3.mp3')
+            tom3.play()  
+        break
+
+        case "d":
+            const tom4 = new Audio('./sounds/tom-4.mp3')
+            tom4.play()  
+        break
+
+        case "j":
+            const snare = new Audio('./sounds/snare.mp3')
+            snare.play()
+        break
+
+        case "k":
+            const crash = new Audio('./sounds/crash.mp3')
+            crash.play()
+        break
+
+        case "l":
+            const kick = new Audio('./sounds/kick-bass.mp3')
+            kick.play()
+        break
+
+        default: console.log(key)
+    }
+}
